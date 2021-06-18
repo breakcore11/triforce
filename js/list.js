466d2fafa1eb11529f711b151ffdf57ec50d3e7e
demonlist <76852729+demonlist@users.noreply.github.com>
2021-06-18 15:20:24 +0300

    first commit

---
+++ js/list.js
@@ -0,0 +1,214 @@
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+			
+      ],
+			"name": "Breakcores Hell",
+                       "author": "Breakcore",
+                       "more": "none",
+                       "id": 32,
+                       "pass": "366611",
+                       "percentToQualify": 52,
+                       "verificationVid": "https://www.youtube.com/watch?v=TL5T8krwAec",
+                       "key": 0
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			
+      ],
+			"name": "Iron cat",
+                       "author": "Nukie",
+                       "more": "none",
+                       "id": 49,
+                       "pass": "Free Copy",
+                       "percentToQualify": 47,
+                       "verificationVid": "https://www.youtube.com/watch?v=_yIFtLcgEcY",
+                       "key": 1
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			
+      ],
+			"name": "To my fffingers I",
+                       "author": "Breakcore",
+                       "more": "none",
+                       "id": 9,
+                       "pass": "696969",
+                       "percentToQualify": 66,
+                       "verificationVid": "https://www.youtube.com/watch?v=SLLkyqwUmMg",
+                       "key": 2
+		},
+		/*=================================================================================*/
+		{
+		"vids": [
+
+			],
+                       "name": "Nebula",
+                       "author": "Nukie",
+                       "more": "none",
+                       "id": 15,
+                       "pass": "696969",
+                       "percentToQualify": 45,
+                       "verificationVid": "https://www.youtube.com/watch?v=rh-FAxU610c",
+                       "key": 3
+                },
+		/*=================================================================================*/
+		{
+			"vids": [
+	
+			],
+			"name": "InUndation",
+			"author": "Nukie and Breakcore [Nukie]",
+			"more": "Breakcore",
+			"id": 41,
+			"pass": "Free Copy",
+			"percentToQualify": 68,
+			"verificationVid": "https://www.youtube.com/watch?v=c1GJXyB3bsE",
+			"key": 4
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+	
+				],
+						   "name": "Daddy",
+						   "author": "Nukie",
+						   "more": "none",
+						   "id": 19,
+						   "pass": "Free Copy",
+						   "percentToQualify": 69,
+						   "verificationVid": "https://www.youtube.com/watch?v=fmtU4VRSQuE",
+						   "key": 5
+					},
+			/*=================================================================================*/
+		{
+		"vids": [
+
+			],
+                       "name": "Long Time Friends",
+                       "author": "Nukie",
+                       "more": "none",
+                       "id": 35,
+                       "pass": "Free Copy",
+                       "percentToQualify": 76,
+                       "verificationVid": "https://www.youtube.com/watch?v=sjNUDs7wiW4",
+                       "key": 6
+                },
+		/*=================================================================================*/
+			{
+				"vids": [
+					{
+						"user": "Breakcore",
+						"link": "https://www.youtube.com/watch?v=S3-kbAxswm8",
+						"percent": 100,
+						"hz": "300Hz"
+					},
+					],
+							   "name": "Atmosphere",
+							   "author": "Nukie",
+							   "more": "none",
+							   "id": 12,
+							   "pass": "Free Copy",
+							   "percentToQualify": 87,
+							   "verificationVid": "https://www.youtube.com/watch?v=YY7dxmadi7Y",
+							   "key": 7
+						},
+				/*=================================================================================*/
+		{
+		"vids": [
+
+			],
+                       "name": "GrAYISH Scale",
+                       "author": "Virpin and Nukie [Nukie]",
+                       "more": "none",
+                       "id": 47,
+                       "pass": "Free Copy",
+                       "percentToQualify": 100,
+                       "verificationVid": "https://www.youtube.com/watch?v=V3Yi8cOx_fA",
+                       "key": 8
+                },
+		/*=================================================================================*/
+						{
+					"vids": [
+			
+						],
+								   "name": "livello blu",
+								   "author": "Breakcore",
+								   "more": "none",
+								   "id": 53,
+								   "pass": "Can't Copy",
+								   "percentToQualify": 100,
+								   "verificationVid": "https://www.youtube.com/watch?v=2AMRArGq1JM",
+								   "key": 9
+							},
+					/*=================================================================================*/
+				{
+					"vids": [
+			
+						],
+								   "name": "SuccY",
+								   "author": "Nukie and Breakcore [Nukie]",
+								   "more": "none",
+								   "id": 16,
+								   "pass": "555555",
+								   "percentToQualify": 100,
+								   "verificationVid": "https://www.youtube.com/watch?v=pHnlPkol8_4",
+								   "key": 10
+							},
+					/*=================================================================================*/
+		{
+						"vids": [
+							{
+								"user": "Breakcore",
+								"link": "https://www.youtube.com/watch?v=tNncl0ujEpU",
+								"percent": 100,
+								"hz": "300Hz"
+							},
+							],
+									   "name": "Time Leaper",
+									   "author": "Nukie",
+									   "more": "none",
+									   "id": 18,
+									   "pass": "Free Copy",
+									   "percentToQualify": 100,
+									   "verificationVid": "https://www.youtube.com/watch?v=Nsf8iuPxXlY",
+									   "key": 11
+								},
+						/*=================================================================================*/
+					{
+						"vids": [
+							{
+								"user": "Breakcore",
+								"link": "https://www.youtube.com/watch?v=ObwD5ziGkP8",
+								"percent": 100,
+								"hz": "300Hz"
+							},
+							],
+									   "name": "Awakening",
+									   "author": "Nukie",
+									   "more": "none",
+									   "id": 10,
+									   "pass": "Free Copy",
+									   "percentToQualify": 100,
+									   "verificationVid": "https://www.youtube.com/watch?v=FZIhhWkrs-c",
+									   "key": 12
+								},
+						/*=================================================================================*/
+
+
+
+
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Hello. This is a copy of Electro Shitty list. It's for a private server.
+---
+yeah thats it.
